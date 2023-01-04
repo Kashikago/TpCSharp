28f80cbd0a1fed68819f5a083cbb9f28aedd073f
Hugo Leang <leang.hugo333@gmail.com>
2023-01-04 15:59:06 +0100

    Automapper profile for id

--- src/AutoMapperProfile.cs
+++ src/AutoMapperProfile.cs
@@ -10,6 +10,7 @@ namespace MinisAPI.Models.DTO
         public AutoMapperProfile()
         {
             CreateMap<PaintItem, PaintItemDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.HexCode, opt => opt.MapFrom(src => src.HexCode))
             .ForMember(dest => dest.Brand, opt => opt.MapFrom(src => src.Brand));
--- src/Models/PaintItemDTO.cs
+++ src/Models/PaintItemDTO.cs
@@ -1,6 +1,7 @@
 namespace MinisAPI.Models.DTO;
 public class PaintItemDTO
 {
+    public long? Id { get; set; }
     public string? Name { get; set; }
     public string? HexCode { get; set; }
     public string? Brand { get; set; }
